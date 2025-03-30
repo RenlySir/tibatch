89d8e6b82a1ac1167b876b9b8b90523c506e7a84
lan <langongru610@163.com>
2025-03-30 10:21:27 +0800

    2

--- method/insert2bypk3.go
+++ method/insert2bypk3.go
@@ -32,7 +32,8 @@ func Insert2ByPK23(db *sql.DB, databaseName string, tableName string, targetData
 }
 
 // 获取分页元数据
-func getPageMetadata23(db *sql.DB, primaryKeyColumns string, pageSize int, databaseName string, tableName string, whereCondition string) []PageMeta23 {
+func getPageMetadata23(db *sql.DB, primaryKeyColumns string, pageSize int, databaseName string,
+	tableName string, whereCondition string) []PageMeta23 {
 	// 直接切割主键列（简单处理，不验证格式）
 	pkCols := strings.Split(primaryKeyColumns, ",")
 	for i := range pkCols {
@@ -58,7 +59,7 @@ func getPageMetadata23(db *sql.DB, primaryKeyColumns string, pageSize int, datab
 				) AS mvalue,
 				%s, %s, %s,
 				ROW_NUMBER() OVER(ORDER BY %s, %s, %s) AS row_num
-			FROM %s.%s %S
+			FROM %s.%s %s
 		) t1
 		GROUP BY page_num
 		ORDER BY page_num`,
@@ -71,6 +72,7 @@ func getPageMetadata23(db *sql.DB, primaryKeyColumns string, pageSize int, datab
 		pkCols[0], pkCols[1], pkCols[2], // ORDER BY
 		databaseName, tableName, whereCondition,
 	)
+	fmt.Printf("分页SQL： %s", query)
 
 	rows, err := db.Query(query)
 	if err != nil {
@@ -195,11 +197,11 @@ func insertPage23(db *sql.DB, page PageMeta23, databaseName string, tableName st
 			%s`, targetDatabaseName, targetTableName, selectColumns, onClause, databaseName, tableName,
 		whereClause, primaryKeyColumns, generateUpdateClause(primaryKeyColumns))
 
-	//fmt.Printf("拼接的SQL：", query)
-	_, err := db.Exec(query)
-	if err != nil {
-		log.Printf("插入分页%d失败: %v", page.PageNum, err)
-		return
-	}
+	fmt.Printf("拼接的SQL：", query)
+	//_, err := db.Exec(query)
+	//if err != nil {
+	//	log.Printf("插入分页%d失败: %v", page.PageNum, err)
+	//	return
+	//}
 	log.Printf("成功插入分页%d, 行数%d", page.PageNum, page.PageSize)
 }
